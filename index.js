3d147a27bfb18c3c5cab016ad1ee447fccf9ef6a
ethanclawsie <ethanclawsie@gmail.com>
2022-10-10 12:41:16 -0700

    Updated files

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require("node:fs");
 const path = require("node:path");
-const { Client, Collection, Intents } = require("discord.js");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, "commands");
@@ -22,7 +22,7 @@ client.once("ready", () => {
 });
 
 client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) return;
+  if (!interaction.isChatInputCommand()) return;
 
   const command = client.commands.get(interaction.commandName);
 
@@ -35,22 +35,6 @@ client.on("interactionCreate", async (interaction) => {
     await interaction.reply({
       content: "There was an error while executing this command!",
       ephemeral: true,
-    });
-    client.on("message", (message) => {
-      if (message.content == `!collect`) {
-        // Create a message collector
-        const filter = (m) =>
-          m.content.includes("discord") && m.author.id != client.user.id;
-        const channel = message.channel;
-        const collector = channel.createMessageCollector(filter, {
-          time: 10000,
-        });
-        console.log("collector started");
-        collector.on("collect", (m) => console.log(`Collected ${m.content}`));
-        collector.on("end", (collected) =>
-          console.log(`Collected ${collected.size} items`)
-        );
-      }
     });
   }
 });
